8dabb9240c0b31de49fcaf2eb12fd2df8729aa1f
Daniel <d_freiermu14@cs.uni-kl.de>
2026-09-06 23:06:30 +0200

    deps: upgrade kotlinx-serialization-json from 1.6.0 to 1.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -207,7 +207,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     
     // Kotlinx Serialization (for all API communication)
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     
     // Security (for Copilot library - EncryptedSharedPreferences)
