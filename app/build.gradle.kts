78650a0bbd54e0b97e96dc0560d3372ecb180f78
Daniel <d_freiermu14@cs.uni-kl.de>
2026-09-08 21:52:31 +0200

    deps: upgrade logging-interceptor from 4.12.0 to 5.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -202,7 +202,7 @@ dependencies {
     
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.5.0")
     
     // Kotlinx Serialization (for all API communication)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
